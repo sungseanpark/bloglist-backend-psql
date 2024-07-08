7cce49d2d08d228399fa87e93f546b390e629d06
sungseanpark <sungseanpark@gmail.com>
2024-07-07 20:20:35 -0700

    Complete part 13.5

---
+++ controllers/blogs.js
@@ -0,0 +1,45 @@
+const router = require('express').Router()
+
+const { Blog } = require('../models')
+
+router.get('/', async (req, res) => {
+    const blogs = await Blog.findAll()
+
+    console.log(JSON.stringify(blogs, null, 2))
+
+    res.json(blogs)
+})
+
+
+router.post('/', async (req, res) => {
+    try {
+      const blog = await Blog.create(req.body)
+      return res.json(blog)
+    } catch(error) {
+      return res.status(400).json({ error })
+    }
+  })
+
+const blogFinder = async (req, res, next) => {
+    req.blog = await Blog.findByPk(req.params.id)
+    next()
+}
+
+router.get('/:id', blogFinder, async (req, res) => {
+    if (req.blog) {
+      res.json(req.blog)
+    } else {
+      res.status(404).end()
+    }
+  })
+  
+router.delete('/:id', blogFinder, async (req, res) => {
+if (req.blog) {
+    await req.blog.destroy()
+}
+res.status(204).end()
+})
+  
+
+
+module.exports = router
