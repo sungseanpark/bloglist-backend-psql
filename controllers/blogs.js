373639925e05e01b6c958670db2bb789c03e93ab
sungseanpark <sungseanpark@gmail.com>
2024-07-14 18:07:19 -0700

    Complete part 13.14

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -21,11 +21,22 @@ const tokenExtractor = (req, res, next) => {
 }
 
 router.get('/', async (req, res) => {
-  const where = {}
+  let where = {}
 
   if (req.query.search) {
-    where.title = {
-      [Op.substring]: req.query.search
+    where = {
+      [Op.or]:[
+        {
+          title: {
+            [Op.substring]: req.query.search
+          }
+        },
+        {
+          author: {
+            [Op.substring]: req.query.search
+          }
+        }
+      ]
     }
   }
 
