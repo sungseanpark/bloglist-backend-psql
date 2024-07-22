f7d5760d48b7ff6cb15147a2140e36a7e50f799e
sungseanpark <sungseanpark@gmail.com>
2024-07-21 17:55:23 -0700

    Complete part 13.20

---
+++ controllers/reading_lists.js
@@ -0,0 +1,45 @@
+const router = require('express').Router()
+
+const { ReadingList } = require('../models')
+
+// router.get('/', async (req, res) => {
+//     const users = await User.findAll({
+//         include: {
+//           model: Blog,
+//           attributes: { exclude: ['userId'] }
+//         }
+//       })
+//   res.json(users)
+// })
+
+router.post('/', async (req, res) => {
+    const reading_list = await ReadingList.create(req.body)
+    res.json(reading_list)
+})
+
+// router.put('/:username', async (req, res) => {
+//   const user = await User.findOne({
+//     where: {
+//       username: req.params.username
+//     }
+//   })
+//     if (user) {
+//         user.username = req.body.username
+//         await user.save()
+//         res.json(user)
+//     }
+//     else {
+//         res.status(404).end()
+//     }
+// })
+
+// router.get('/:id', async (req, res) => {
+//   const user = await User.findByPk(req.params.id)
+//   if (user) {
+//     res.json(user)
+//   } else {
+//     res.status(404).end()
+//   }
+// })
+
+module.exports = router
--- controllers/users.js
+++ controllers/users.js
@@ -4,6 +4,7 @@ const { User, Blog } = require('../models')
 
 router.get('/', async (req, res) => {
     const users = await User.findAll({
+      attributes: { exclude: ['createdAt', 'updatedAt'] },
         include: {
           model: Blog,
           attributes: { exclude: ['userId'] }
@@ -33,13 +34,30 @@ router.put('/:username', async (req, res) => {
     }
 })
 
-// router.get('/:id', async (req, res) => {
-//   const user = await User.findByPk(req.params.id)
-//   if (user) {
-//     res.json(user)
-//   } else {
-//     res.status(404).end()
-//   }
-// })
+router.get('/:id', async (req, res) => {
+  const user = await User.findByPk(req.params.id, {
+    attributes: { exclude: ['id', 'createdAt', 'updatedAt'] } ,
+    include:[
+      {
+        model: Blog,
+        as: 'readings',
+        attributes: { exclude: ['userId', 'createdAt', 'updatedAt']},
+        through: {
+          attributes: []
+        },
+        // include: {
+        //   model: User,
+        //   attributes: ['name']
+        // }
+      },
+    ]
+  })
+
+  if (user) {
+    res.json(user)
+  } else {
+    res.status(404).end()
+  }
+})
 
 module.exports = router
