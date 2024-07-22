b538a17cdd175026ff574cc4c754d325e9175a1f
sungseanpark <sungseanpark@gmail.com>
2024-07-21 17:25:06 -0700

    Complete part 13.19

--- models/index.js
+++ models/index.js
@@ -1,11 +1,13 @@
 const Blog = require('./blog')
 const User = require('./user')
+const ReadingList = require('./reading_list')
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
-// Blog.sync({ alter: true })
-// User.sync({ alter: true })
+
+User.belongsToMany(Blog, { through: ReadingList, as: 'readings' })
+Blog.belongsToMany(User, { through: ReadingList, as: 'readinglists' })
 
 module.exports = {
-  Blog, User
+  Blog, User, ReadingList
 }
